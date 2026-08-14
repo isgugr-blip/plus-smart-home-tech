a977bb59daceae1513258ee76039dfbcf4ccb35e
Iskender Mamedov <i.mamedov@hh.ru>
2026-08-14 18:30:20 +0700

    collector

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApp.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/CollectorApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CollectorApp {
-
     public static void main(String[] args) {
         SpringApplication.run(CollectorApp.class, args);
     }
