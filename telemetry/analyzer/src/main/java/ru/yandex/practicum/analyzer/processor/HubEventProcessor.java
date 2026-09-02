9cb4b4aa79db8cdce2555d50209b774f4f857b6d
Iskender Mamedov <i.mamedov@hh.ru>
2026-09-02 14:49:01 +0700

    review fixes

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/processor/HubEventProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/processor/HubEventProcessor.java
@@ -4,14 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.analyzer.kafka.KafkaProperties;
 import ru.yandex.practicum.analyzer.service.HubEventService;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 
-import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -19,42 +22,58 @@ public class HubEventProcessor implements Runnable {
 
     private final Consumer<String, HubEventAvro> consumer;
     private final HubEventService hubEventService;
-    private final String topic;
-    private final Duration pollTimeout;
+    private final KafkaProperties properties;
+    private final Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>();
 
     public HubEventProcessor(Consumer<String, HubEventAvro> hubEventConsumer,
                              HubEventService hubEventService,
-                             @Value("${analyzer.kafka.hubs-topic}") String topic,
-                             @Value("${analyzer.kafka.poll-timeout}") Duration pollTimeout) {
+                             KafkaProperties properties) {
         this.consumer = hubEventConsumer;
         this.hubEventService = hubEventService;
-        this.topic = topic;
-        this.pollTimeout = pollTimeout;
+        this.properties = properties;
     }
 
     @Override
     public void run() {
         Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
         try {
-            consumer.subscribe(List.of(topic));
-            log.info("Подписались на топик [{}]", topic);
+            consumer.subscribe(List.of(properties.hubsTopic()));
+            log.info("Подписались на топик [{}]", properties.hubsTopic());
 
             while (true) {
-                ConsumerRecords<String, HubEventAvro> records = consumer.poll(pollTimeout);
+                ConsumerRecords<String, HubEventAvro> records = consumer.poll(properties.pollTimeout());
+                boolean processed = false;
                 for (ConsumerRecord<String, HubEventAvro> record : records) {
                     try {
                         hubEventService.handle(record.value());
                     } catch (Exception e) {
-                        log.error("Не удалось обработать событие хаба {}", record.value(), e);
+                        log.error("Не удалось обработать событие хаба {}, смещение не фиксируем", record.value(), e);
+                        break;
                     }
+                    offsets.put(new TopicPartition(record.topic(), record.partition()),
+                            new OffsetAndMetadata(record.offset() + 1));
+                    processed = true;
+                }
+                if (processed) {
+                    consumer.commitAsync(offsets, (committed, error) -> {
+                        if (error != null) {
+                            log.warn("Не удалось зафиксировать смещения {}", committed, error);
+                        }
+                    });
                 }
             }
         } catch (WakeupException ignored) {
         } catch (Exception e) {
             log.error("Ошибка во время обработки событий хабов", e);
         } finally {
-            log.info("Закрываем консьюмер событий хабов");
-            consumer.close();
+            try {
+                if (!offsets.isEmpty()) {
+                    consumer.commitSync(offsets);
+                }
+            } finally {
+                log.info("Закрываем консьюмер событий хабов");
+                consumer.close();
+            }
         }
     }
 }
--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/HubServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/HubServiceImpl.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.collector.kafka.KafkaClient;
-import ru.yandex.practicum.collector.kafka.KafkaTopics;
+import ru.yandex.practicum.collector.kafka.KafkaProperties;
 import ru.yandex.practicum.collector.mapper.HubEventMapper;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
@@ -16,14 +16,15 @@ import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 public class HubServiceImpl implements HubService {
 
     private final KafkaClient kafkaClient;
+    private final KafkaProperties properties;
 
     @Override
     public void send(HubEventProto event) {
         HubEventAvro avro = HubEventMapper.toAvro(event);
         log.info("Событие хаба {} (hubId={}) отправляется в топик {}",
-                event.getPayloadCase(), event.getHubId(), KafkaTopics.HUBS);
+                event.getPayloadCase(), event.getHubId(), properties.hubsTopic());
         kafkaClient.getProducer().send(new ProducerRecord<>(
-                KafkaTopics.HUBS,
+                properties.hubsTopic(),
                 null,
                 avro.getTimestamp().toEpochMilli(),
                 avro.getHubId(),
